72234af11c08b8686609a272a6c8e2f349120637
GregPuccio <gregpuccio@gmail.com>
2021-10-10 19:38:24 -0400

    removed env

--- src/Bootstrap/Bootstrap.php
+++ src/Bootstrap/Bootstrap.php
@@ -27,7 +27,7 @@ class Bootstrap
      */
     public static function init()
     {
-        self::initEnvironment();
+        // self::initEnvironment(); commented out to stop initializing .env
         self::initFatFree();
 
         // Load the routes
